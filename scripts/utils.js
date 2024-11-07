d569b2cc2a259f1902e0223125180988d27d1615
Andres Giraldo Gallego <andresgi0521@hotmail.com>
2024-11-06 21:46:54 -0500

    clase card

---
+++ scripts/utils.js
@@ -0,0 +1,26 @@
+export const initialCards = [
+  {
+    name: "Machu Picchu",
+    link: "https://media.istockphoto.com/id/1410796651/es/foto/pareja-vestida-de-ponchos-observando-las-ruinas-de-machu-picchu-per%C3%BA.jpg?s=2048x2048&w=is&k=20&c=1D6GM_8KNtEZWilnl9p7ubLQ3qQMoXIckDJthJqvLEQ=",
+  },
+  {
+    name: "Islandia",
+    link: "https://media.istockphoto.com/id/518400380/es/foto/reykjavik-la-capital-de-islandia.jpg?s=2048x2048&w=is&k=20&c=kxD7meQZ7c4feZ7n262OzWUwRyKwCf5z7AZ7Xts4K-8=",
+  },
+  {
+    name: "Paris",
+    link: "https://media.istockphoto.com/id/1145422105/es/foto/vista-a%C3%A9rea-de-la-torre-eiffel-par%C3%ADs.jpg?s=2048x2048&w=is&k=20&c=QOlXs2IPoHiZWGb6m9_QnPkOFWNU_-izRXbrErDdVOA=",
+  },
+  {
+    name: "Cataratas del Niagara",
+    link: "https://media.istockphoto.com/id/164193085/es/foto/oto%C3%B1o-en-las-cataratas-del-ni%C3%A1gara.jpg?s=1024x1024&w=is&k=20&c=0Yp7Xfck6edLOtN74N7NUqo42qH35DAUnhRSwsXkULk=",
+  },
+  {
+    name: "Estatua de la Libertad",
+    link: "https://media.istockphoto.com/id/2160923465/es/foto/estatua-de-la-libertad-bandera-estadounidense-y-horizonte-de-nueva-york.jpg?s=2048x2048&w=is&k=20&c=luvx0T2gfP3FOz9KFFb7alw_IlS3nT7kiz4eOWc4PaI=",
+  },
+  {
+    name: "San Andres Isla",
+    link: "https://s3.amazonaws.com/rtvc-assets-senalcolombia.gov.co/s3fs-public/styles/imagen_noticia/public/field/image/San-andres-RTVC.jpg?itok=iVcNPiNw",
+  },
+];
