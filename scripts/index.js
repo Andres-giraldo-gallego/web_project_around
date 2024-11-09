daa7fdee30253f755db4c6e94c30fbe4110fa099
Andres Giraldo Gallego <andresgi0521@hotmail.com>
2024-11-09 13:54:24 -0500

    forvalidator

--- scripts/index.js
+++ scripts/index.js
@@ -11,7 +11,6 @@ const formElement = document.querySelector(".popup__form");
 const profileName = document.querySelector(".profile_name");
 const profileHobbie = document.querySelector(".profile_hobbie");
 const carTemplate = document.querySelector("#elements_template").content;
-const cardsContainer = document.querySelector(".elements_container");
 const buttonAdd = document.querySelector(".profile_add-button");
 const popupCard = document.querySelector("#popup-card");
 const buttonCrear = document.querySelector("#popup-card-Submit-Button");
@@ -20,8 +19,6 @@ const cardLink = document.querySelector("#popup-link");
 const buttonX = document.querySelector("#popup-x");
 const dialogImg = document.querySelector(".dialog__content");
 const dislogText = document.querySelector(".dialog__text");
-const imgDialog = document.querySelector("#dialog-img");
-const textDialog = document.querySelector("#dialog-text");
 const closeButtons = document.querySelectorAll(".popup__close-btn");
 const closeButtonImg = document.querySelector(".dialog__button");
 
@@ -53,8 +50,9 @@ buttonCrear.addEventListener("click", function (e) {
   popupCard.classList.remove("popup_opened");
   const name = cardName.value;
   const link = cardLink.value;
-  const cardElement = createCard({ name, link });
-  cardsContainer.prepend(cardElement);
+  const elements = document.querySelector(".elements_container");
+  const card = new Card(name, link);
+  const cardElement = elements.prepend(card.createCard());
 });
 
 buttonGuardar.addEventListener("click", function (e) {
@@ -79,46 +77,10 @@ initialCards.forEach((item) => {
   const cardElement = elements.prepend(card.createCard());
 });
 
-function createCard(card) {
-  let cards = carTemplate.querySelector(".elements_card").cloneNode(true);
-  cards.querySelector(".elements_img").src = card.link;
-  cards.querySelector(".elements_text").textContent = card.name;
-  deleteElementsTrash(cards);
-  toggleElementsLink(cards);
-  openCardImages(card, cards);
-  return cards;
-}
-
-// Sobre escribir imagen
-
-function openCardImages(card, cards) {
-  cards.querySelector(".elements_img").addEventListener("click", function () {
-    dialogImg.showModal();
-    imgDialog.src = card.link;
-    textDialog.textContent = card.name;
-  });
-}
 closeButtonImg.addEventListener("click", function (evt) {
   evt.preventDefault();
   dialogImg.close();
 });
-
-// botton borrar carta
-
-function deleteElementsTrash(card) {
-  let buttonTrash = card.querySelector(".elements__trash-img");
-  buttonTrash.addEventListener("click", function () {
-    card.remove();
-  });
-}
-// Botton de me gusta
-
-function toggleElementsLink(card) {
-  let buttonLink = card.querySelector(".elements_link-img");
-  buttonLink.addEventListener("click", function () {
-    buttonLink.classList.toggle("elements_link-img-active");
-  });
-}
 
 // Cerrar por fuera del formulario
 popupContainer.addEventListener("click", function (evt) {
@@ -154,14 +116,14 @@ dialogImg.addEventListener("click", function (evt) {
 // cerrar popup con la letra esc
 
 function precionar_tecla(evt) {
-  teclas_esc = evt.key;
+  const teclas_esc = evt.key;
   if (teclas_esc == "Escape") {
     return closePopupCard();
   }
 }
 
 function precionar_Esc(evt) {
-  teclas_esc = evt.key;
+  const teclas_esc = evt.key;
   if (teclas_esc == "Escape") {
     return closePopupContainer();
   }
