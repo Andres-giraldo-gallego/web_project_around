57c851f329c2bbb71104c5024d66226352f7b4b6
Andres Giraldo Gallego <andresgi0521@hotmail.com>
2024-12-10 19:52:45 -0500

    sprint 12 api

--- components/PopupWithForm.js
+++ components/PopupWithForm.js
@@ -24,14 +24,29 @@ export default class PopupWithForm extends Popup {
     return values;
   }
 
+  renderLogin(isLoading, botonLoading, originalText) {
+    console.log(originalText);
+    if (isLoading) {
+      botonLoading.textContent = "Cargando...";
+    } else {
+      botonLoading.textContent = originalText;
+    }
+    setTimeout(() => {}, 3000);
+  }
+
   setEventListeners() {
     super.setEventListeners();
     const form = this.popup.querySelector(".popup__form");
+    const botonLoading = this.popup.querySelector(".button-loading");
+    const originalText = botonLoading.textContent;
     form.addEventListener("submit", (evt) => {
       evt.preventDefault();
+      this.renderLogin(true, botonLoading, originalText);
       const data = this.getInputValues();
-      this.handleForsubmit(data);
-      this.close();
+      this.handleForsubmit(data).then(() => {
+        this.renderLogin(false, botonLoading, originalText);
+        this.close();
+      });
     });
   }
 }
